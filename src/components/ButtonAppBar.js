f60caece598273b30241dffab019e7a8e7783119
Anuj Gakhar <anuj.gakhar@gmail.com>
2020-09-03 10:39:30 +0100

    white bg

--- src/components/ButtonAppBar.js
+++ src/components/ButtonAppBar.js
@@ -6,7 +6,7 @@ import Typography from '@material-ui/core/Typography';
 export default function ButtonAppBar() {
   return (
     <div>
-      <AppBar position="static">
+      <AppBar position="static" color="secondary">
         <Toolbar>
           <Typography variant="h6">Name/Logo goes here</Typography>
         </Toolbar>
