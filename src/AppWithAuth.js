4c4527217e98d4fec56961fc591954352f849d68
Anuj Gakhar <anuj.gakhar@gmail.com>
2020-11-08 17:48:45 +0000

    more updates

--- src/AppWithAuth.js
+++ src/AppWithAuth.js
@@ -1,6 +1,7 @@
-import { AmplifySignIn, AmplifyAuthenticator } from '@aws-amplify/ui-react';
+import { SignIn, Authenticator } from 'aws-amplify-react';
 import App from './App';
 import React from 'react';
+import '@aws-amplify/ui/dist/style.css';
 
 const federated = {
   google_client_id: '812957620541-ea9mak245pm437uheokbtdp9i3eu633i.apps.googleusercontent.com', // Enter your google_client_id here
@@ -8,12 +9,21 @@ const federated = {
   amazon_client_id: '', // Enter your amazon_client_id here
 };
 
+const theme = {
+  signInButtonIcon: { display: 'none' },
+};
+
+const signUpConfig = {
+  hiddenDefaults: ['phone_number', 'username'],
+  signUpFields: [{ label: 'Name', key: 'name', required: true, type: 'string', displayOrer: 1 }],
+};
+
 const AppWithAuth = () => {
   return (
-    <AmplifyAuthenticator hide={[AmplifySignIn]} federated={federated}>
-      <AmplifySignIn />
+    <Authenticator federated={federated} theme={theme} signUpConfig={signUpConfig}>
+      <SignIn />
       <App />
-    </AmplifyAuthenticator>
+    </Authenticator>
   );
 };
 
