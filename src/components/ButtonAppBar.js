2ded3b32037ddb768bfbf77ae57d7fbf84cff748
Anuj Gakhar <anuj.gakhar@gmail.com>
2020-11-20 13:59:37 +0000

    removing the signout button temporarily from the header

--- src/components/ButtonAppBar.js
+++ src/components/ButtonAppBar.js
@@ -2,7 +2,6 @@ import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import { AmplifySignOut } from '@aws-amplify/ui-react';
 
 export default function ButtonAppBar() {
   return (
@@ -10,7 +9,6 @@ export default function ButtonAppBar() {
       <AppBar position="static" color="transparent">
         <Toolbar>
           <Typography variant="h6">Name/Logo goes here</Typography>
-          <AmplifySignOut />
         </Toolbar>
       </AppBar>
     </div>
