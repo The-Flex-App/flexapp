aa941db6e05f9afb7db17e6a260fa983397c26f2
Anuj Gakhar <anuj.gakhar@gmail.com>
2021-03-06 17:59:53 +0000

    fix sidebar and content width

--- src/App.js
+++ src/App.js
@@ -12,11 +12,13 @@ function App() {
       <Grid item xs={12}>
         <ButtonAppBar />
       </Grid>
-      <Grid item xs={2} className="project-list">
-        <Projects />
-      </Grid>
-      <Grid item xs={10} className="videos-list">
-        <Videos />
+      <Grid item xs={12} className="app-wrapper">
+        <div className="sidebar">
+          <Projects />
+        </div>
+        <div className="content-area">
+          <Videos />
+        </div>
       </Grid>
       <Grid item xs={12}>
         <Paper>
